7eabc1367d9f7c1e8d6b09b81fad36982a93daa8
Andy Ball <932400399@desktop-cnc8ria.acg.ac.nz>
2019-03-12 14:04:32 +1300

    stopped for a bit for the tutorial

--- Input&Events EventObject/www/appscripts/main.js
+++ Input&Events EventObject/www/appscripts/main.js
@@ -2,7 +2,19 @@ require(
         [],
         function () {
 
-        	console.log("yo");
+        	console.log("welcome to my event object homework journey");
+
+        	var colorbox = document.getElementById("colorDisplay");
+
+        	colorbox.addEventListener("mousedown", function(ev){ /* which bit here is the callback function ?? */
+        		console.log("okay, got a mousedown event");
+        		console.log("the target is " + ev.target);
+        		console.log("the target's ID is " + ev.target.id);
+
+        		ev.target.innerHTML = "The mouse's offset X was " + ev.offsetX + /* we need to finish defining this bad baby */ 
+        	})
 
         }
+
+
 );
